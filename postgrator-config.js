3ff635e194797f288789a20475828d510ec18fdb
acheckler <allisonheckler@gmail.com>
2021-08-26 22:11:19 -0500

    heroku fixes

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,5 +6,11 @@ module.exports = {
   "native": "true",
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL
+    : process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 }
+const postgrator = new Postgrator(options)
+postgrator.on('validation-started', (migration) => console.log(migration))
+postgrator.on('validation-finished', (migration) => console.log(migration))
+postgrator.on('migration-started', (migration) => console.log(migration))
+postgrator.on('migration-finished', (migration) => console.log(migration))
